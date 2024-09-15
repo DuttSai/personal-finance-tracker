4f8bc6c3e383a663a6b5a77faa649c59617c6e4e
Saidutt Nagumanthri <saidutt49@gmail.com>
2024-09-16 00:05:51 +0530

    updated-route-links-and-main-css

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Login from './components/login/login';
 import Home from './components/Home/Home';
 import { Register } from './components/Register/Register';
 import { Dashboard } from './components/Dashboard/Dashboard';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className='main'>
+      <nav>
+        <ul>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/Register'>Register</Link></li>
+          <li><Link to='/Dashboard'>Dashboard</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path='*' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,7 @@
-import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/Register'>Register</Link></li>
-          <li><Link to='/Dashboard'>Dashboard</Link></li>
-        </ul>
-      </nav>
       <h1>Home</h1>
     </div>
   );
